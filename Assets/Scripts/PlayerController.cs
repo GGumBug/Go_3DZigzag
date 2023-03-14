6dabe020dce2fc3cfcdea1eafe76fcf1356e35a1
GGumBug <alsrl7538@gmail.com>
2023-03-14 20:41:06 +0900

    게임종료 로직 추가 및 시작 조건 추가

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,29 +4,49 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private Movement    movement;
-    private float       limitDeathY;
+    [SerializeField]
+    private GameController  gameController;
+    private Movement        movement;
+    private float           limitDeathY;
 
     private void Awake() {
         movement = GetComponent<Movement>();
-
-        movement.MoveTo(Vector3.right);
 
         // 1.65 - 0.3 * 0.5 = 1.5
         limitDeathY = transform.position.y - transform.localScale.y * 0.5f;
     }
 
+    private IEnumerator Start()
+    {
+        while (true)
+        {
+            if (gameController.IsGameStart)
+            {
+                movement.MoveTo(Vector3.right);
+
+                yield break;
+            }
+
+            yield return null;
+        }
+    }
+
     private void Update() {
+
+        if (gameController.IsGameOver) return;
+
         if (Input.GetMouseButtonDown(0))
         {
             Vector3 direction = movement.MoveDirection == Vector3.forward ? Vector3.right : Vector3.forward;
 
             movement.MoveTo(direction);
+
+            gameController.IncreaseScore();
         }
 
         if (transform.position.y < limitDeathY)
         {
-            Debug.Log("GameOver");
+            gameController.GameOver();
         }
     }
 }
